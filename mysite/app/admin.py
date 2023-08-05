19b55bc569fbf5b9172706a6bfdfe81f4e48e6b2
TITW52 <etarasov2007@gmail.com>
2023-08-05 12:20:33 +0300

    лекция 8дз

--- mysite/app/admin.py
+++ mysite/app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+    list_filter = ['auction', "created_at"]
+    actions = ["MakeAuctionAsFalse", "MakeAuctionAsTrue"]
+    fieldsets = (
+        (
+            'Общее', {"fields": ('title', 'description')}
+        ),
+        (
+            'Финансы', {"fields": ('price', 'auction'), "classes": ["collapse"]}
+        ),
+    )
+    
+    @admin.action(description="убрать торг")
+    def MakeAuctionAsFalse(self,request, quaryset):
+        quaryset.update(auction=False)
+        
+    @admin.action(description="добавить торг")
+    def MakeAuctionAsTrue(self,request, quaryset):
+        quaryset.update(auction=True)
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+
